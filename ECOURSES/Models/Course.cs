eccb2efaf72ce72088c740a8a89f9edcb724afb0
YOUSSEF-YL <youssef.bachar7@gmail.com>
2022-07-25 22:24:31 +0100

    Teacherclass

--- ECOURSES/Models/Course.cs
+++ ECOURSES/Models/Course.cs
@@ -7,11 +7,11 @@ namespace ECOURSES.Models
         [Key]
         public int CourseID { get; set; }
         public string? CourseName { get; set; }
-
         public Decimal Price { get; set; }
         public int  Duration{ get; set; }
         public int TeacherID { get; set; }
-
-        public List<Student>? subdeStudent { get; set; } 
+        public string? Imag { get; set; }
+       
+        public List<Student>? SubdeStudent { get; set; } 
     }
 }
