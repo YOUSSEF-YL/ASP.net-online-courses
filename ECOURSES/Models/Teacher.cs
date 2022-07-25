fe0499cb0b9d6305468f4c349b5a68d31866babe
YOUSSEF-YL <youssef.bachar7@gmail.com>
2022-07-25 22:48:50 +0100

    Teacher

--- ECOURSES/Models/Teacher.cs
+++ ECOURSES/Models/Teacher.cs
@@ -2,5 +2,14 @@
 {
     public class Teacher
     {
+
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Imag { get; set; }
+        public string Subjects { get; set; }
+        public string? Facebook { get; set; }
+        public string? Linkedin { get; set; }
+        public string? Twitter { get; set; }
     }
 }
