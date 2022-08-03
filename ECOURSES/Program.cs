6b12ed8551521777e043586e386b64352468fe42
YOUSSEF-YL <youssef.bachar7@gmail.com>
2022-08-03 21:54:36 +0100

    Set-Jwt

--- ECOURSES/Program.cs
+++ ECOURSES/Program.cs
@@ -1,17 +1,51 @@
 using ECOURSES.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// EntityFrameworkCore
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
 )) ;
+// JwtBearer
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audiences"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+        };
+    });
+builder.Services.AddMvc();
+builder.Services.AddControllers();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
+// to accses api controllers
+
+app.UseAuthentication();
+//app.UseEndpoints(endpoint =>
+//{
+//    endpoint.MapControllers();
+//});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
